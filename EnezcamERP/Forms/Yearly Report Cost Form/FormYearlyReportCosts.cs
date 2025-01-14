7e9ab04d8294a0afe29a36133b9edafa6963141e
Cadoewyr <omeryanikdag0@gmail.com>
2025-01-14 12:08:53 +0300

    Fixed fetching monthly outgoings by year.

--- EnezcamERP/Forms/Yearly Report Cost Form/FormYearlyReportCosts.cs
+++ EnezcamERP/Forms/Yearly Report Cost Form/FormYearlyReportCosts.cs
@@ -24,18 +24,18 @@ namespace EnezcamERP.Forms.Yearly_Report_Cost_Form
         {
             var res = moRepository.GetAll();
 
-            nudM1.Value = res.Where(x => x.Month == 1).First() != null ? res.Where(x => x.Month == 1).First().Outgoing : 0;
-            nudM2.Value = res.Where(x => x.Month == 2).First() != null ? res.Where(x => x.Month == 2).First().Outgoing : 0;
-            nudM3.Value = res.Where(x => x.Month == 3).First() != null ? res.Where(x => x.Month == 3).First().Outgoing : 0;
-            nudM4.Value = res.Where(x => x.Month == 4).First() != null ? res.Where(x => x.Month == 4).First().Outgoing : 0;
-            nudM5.Value = res.Where(x => x.Month == 5).First() != null ? res.Where(x => x.Month == 5).First().Outgoing : 0;
-            nudM6.Value = res.Where(x => x.Month == 6).First() != null ? res.Where(x => x.Month == 6).First().Outgoing : 0;
-            nudM7.Value = res.Where(x => x.Month == 7).First() != null ? res.Where(x => x.Month == 7).First().Outgoing : 0;
-            nudM8.Value = res.Where(x => x.Month == 8).First() != null ? res.Where(x => x.Month == 8).First().Outgoing : 0;
-            nudM9.Value = res.Where(x => x.Month == 9).First() != null ? res.Where(x => x.Month == 9).First().Outgoing : 0;
-            nudM10.Value = res.Where(x => x.Month == 10).First() != null ? res.Where(x => x.Month == 10).First().Outgoing : 0;
-            nudM11.Value = res.Where(x => x.Month == 11).First() != null ? res.Where(x => x.Month == 11).First().Outgoing : 0;
-            nudM12.Value = res.Where(x => x.Month == 12).First() != null ? res.Where(x => x.Month == 12).First().Outgoing : 0;
+            nudM1.Value = res.Where(x => x.Month == 1 & x.Year == _year).Any() ? res.Where(x => x.Month == 1 & x.Year == _year).First().Outgoing : 0;
+            nudM2.Value = res.Where(x => x.Month == 2 & x.Year == _year).Any() ? res.Where(x => x.Month == 2 & x.Year == _year).First().Outgoing : 0;
+            nudM3.Value = res.Where(x => x.Month == 3 & x.Year == _year).Any() ? res.Where(x => x.Month == 3 & x.Year == _year).First().Outgoing : 0;
+            nudM4.Value = res.Where(x => x.Month == 4 & x.Year == _year).Any() ? res.Where(x => x.Month == 4 & x.Year == _year).First().Outgoing : 0;
+            nudM5.Value = res.Where(x => x.Month == 5 & x.Year == _year).Any() ? res.Where(x => x.Month == 5 & x.Year == _year).First().Outgoing : 0;
+            nudM6.Value = res.Where(x => x.Month == 6 & x.Year == _year).Any() ? res.Where(x => x.Month == 6 & x.Year == _year).First().Outgoing : 0;
+            nudM7.Value = res.Where(x => x.Month == 7 & x.Year == _year).Any() ? res.Where(x => x.Month == 7 & x.Year == _year).First().Outgoing : 0;
+            nudM8.Value = res.Where(x => x.Month == 8 & x.Year == _year).Any() ? res.Where(x => x.Month == 8 & x.Year == _year).First().Outgoing : 0;
+            nudM9.Value = res.Where(x => x.Month == 9 & x.Year == _year).Any() ? res.Where(x => x.Month == 9 & x.Year == _year).First().Outgoing : 0;
+            nudM10.Value = res.Where(x => x.Month == 10 & x.Year == _year).Any() ? res.Where(x => x.Month == 10 & x.Year == _year).First().Outgoing : 0;
+            nudM11.Value = res.Where(x => x.Month == 11 & x.Year == _year).Any() ? res.Where(x => x.Month == 11 & x.Year == _year).First().Outgoing : 0;
+            nudM12.Value = res.Where(x => x.Month == 12 & x.Year == _year).Any() ? res.Where(x => x.Month == 12 & x.Year == _year).First().Outgoing : 0;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
