fe6ebe7031981719a4d42c8a6c0854930c3c78f7
Cadoewyr <omeryanikdag0@gmail.com>
2025-06-13 15:38:39 +0300

    Added a condition to check if database connection opened or not.

--- EnezcamERP/Program.cs
+++ EnezcamERP/Program.cs
@@ -1,3 +1,6 @@
+using EnezcamERP.Validators;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Diagnostics;
 
 namespace EnezcamERP
@@ -15,8 +18,39 @@ namespace EnezcamERP
             if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(System.Reflection.Assembly.GetEntryAssembly().Location)).Count() > 1)
                 return;
 
+            if (!CheckDatabaseConnection())
+            {
+                MessageBox.Show("Veritabanýna baðlanýlamadý. Uygulama kapatýlýyor.", "Baðlantý Hatasý", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             ApplicationConfiguration.Initialize();
             Application.Run(new FormMain());
+        }
+
+        private static bool CheckDatabaseConnection()
+        {
+            try
+            {
+                IConfiguration _config;
+
+                var builder = new ConfigurationBuilder();
+                builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+                _config = builder.Build();
+
+                using(SqlConnection conn = new SqlConnection(_config["ConnectionStrings:DefaultConnection"]))
+                {
+                    conn.Open();
+                    return conn.State == System.Data.ConnectionState.Open;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Veritabaný baðlantý hatasý: " + ex.Message);
+                return false;
+            }
         }
     }
 }
